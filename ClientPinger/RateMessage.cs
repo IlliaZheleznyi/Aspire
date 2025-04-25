9d42fe5bb22c27ccd75f5b05cefdabf6e7dfef7c
Illia Zheleznyi <IZheleznyi@mtf.org>
2025-04-25 16:18:50 +0100

    Add clients SignalR

---
+++ ClientPinger/RateMessage.cs
@@ -0,0 +1,1 @@
+﻿public record RateMessage(long Timestamp, decimal Rate);
---
+++ ClientPinger1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Net;
+
+Console.WriteLine("Connecting to SignalR");
+
+var cookies = new CookieContainer();
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7200/chathub?userId=User2", options =>
+    {
+        options.HttpMessageHandlerFactory = _ => new HttpClientHandler
+        {
+            CookieContainer = cookies,
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+    })
+    .WithAutomaticReconnect()
+    .Build();
+
+connection.On<RateMessage>("ReceiveMessage", msg =>
+{
+    Console.WriteLine($"[{DateTimeOffset.FromUnixTimeMilliseconds(msg.Timestamp)}] Rate: {msg.Rate}");
+});
+
+await connection.StartAsync();
+Console.WriteLine("Connected to hub");
+
+await connection.InvokeAsync("Subscribe", "User2", 1);
+Console.WriteLine("Sent subscription for metric 1 (TimeRate)");
+
+
+
+
+await Task.Delay(Timeout.Infinite);
